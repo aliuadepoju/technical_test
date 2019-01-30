ffe626672c892c425922effa0eb3d3a4ce5e358b
Lawrence Onah <paplow01@gmail.com>
2019-01-30 10:45:39 +0100

    Minor mmodification

--- app/Helpers/Utility.php
+++ app/Helpers/Utility.php
@@ -2,8 +2,28 @@
 
 namespace App\Helpers;
 
+use App\User;
 
 class Utility
 {
+    /**
+     * Generate unique random api token for the user
+     * @param User $user
+     * @return bool|string
+     */
+    public function generateUniqueToken(User $user)
+    {
+        try {
+            $token = hex2bin(random_bytes(32));
+            $tokenExists = $user->whereApiToken($token)->first();
 
+            if ($tokenExists) {
+                return static::generateUniqueToken($user);
+            }
+
+            return $token;
+        } catch (\Exception $exception) {
+            \Log::error($exception->getMessage());
+        }
+    }
 }
