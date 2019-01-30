b8c720fde7aafd410a5fba754d9ce5f6d86610e7
Lawrence Onah <paplow01@gmail.com>
2019-01-30 17:46:31 +0100

    Completed :rocket:

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +48,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($request->isJson() && $exception instanceof AuthenticationException) {
+            return response()->json(['message' => 'Unauthorized!'], Response::HTTP_UNAUTHORIZED);
+        }
+        
         return parent::render($request, $exception);
     }
 }
