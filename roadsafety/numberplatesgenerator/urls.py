d0690ff46d9d3d771cbbc00aa1170b0635a31286
Eze Sunday Eze <mailsforeze@gmail.com>
2019-02-03 18:26:38 +0100

    Updated API doc

--- roadsafety/numberplatesgenerator/urls.py
+++ roadsafety/numberplatesgenerator/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.documentation import include_docs_urls
 from numberplatesgenerator import views
 router = DefaultRouter()
 router.register("api/v1", views.ViewPlateNumbers,
@@ -9,6 +10,7 @@ router.register("api/v1", views.ViewPlateNumbers,
 urlpatterns = [
     path('', include(router.urls)),
     path('login', views.login_page, name='login'),
+    path('doc', include_docs_urls(title="Road Safey")),
     path('generator', views.number_plate_generator_page, name='generator'),
     path('report', views.report_page, name='report'),
 ]
